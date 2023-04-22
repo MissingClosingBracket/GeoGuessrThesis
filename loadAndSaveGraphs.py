698b6386847a921efcb72ca4971358a3047cbc4e
Christian <cpl@capacit.com>
2023-04-22 13:05:27 +0200

    found a solution to get all islands of malta by combinig using nx.compose_all

--- loadAndSaveGraphs.py
+++ loadAndSaveGraphs.py
@@ -1,9 +1,12 @@
 import osmnx as ox
 
 #example, malta
-G = ox.graph_from_place('Republic of Malta', network_type='drive')
-ox.save_graphml(G, filepath="./savedGraphs/malta.graphml")
-
+G1 = ox.graph_from_bbox(north=36.0831,south=36.0036,east=14.3523, west=14.1825, network_type='drive')
+ox.save_graphml(G1, filepath="./savedGraphs/malta1.graphml")
+G2 = ox.graph_from_bbox(north=36.0002,south=35.8039,east=14.5782, west=14.3183, network_type='drive')
+ox.save_graphml(G2, filepath="./savedGraphs/malta2.graphml")
+G3 = ox.graph_from_bbox(north=36.0209,south=36.0036,east=14.3513, west=14.3162, network_type='drive')
+ox.save_graphml(G3, filepath="./savedGraphs/malta3.graphml")
 #example, bornholm
-G = ox.graph_from_place('Bornholm', network_type='drive')
-ox.save_graphml(G, filepath="./savedGraphs/bornholm.graphml")
+G4 = ox.graph_from_place('Bornholm', network_type='drive')
+ox.save_graphml(G4, filepath="./savedGraphs/bornholm.graphml")
--- simpleGraphDisplay.py
+++ simpleGraphDisplay.py
@@ -2,13 +2,22 @@ import osmnx as ox
 from osmnx.plot import get_edge_colors_by_attr
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import networkx as nx
 
 #Bornholm or malta?
 #place = 'bornholm'
-place = 'malta'
+place = 'bornholm'
 
-#Get graph from bbox
-graph = ox.load_graphml("./savedGraphs/" + place + ".graphml")
+graph = nx.MultiGraph
+
+#Get graph
+if place != 'malta':
+    graph :nx.MultiDiGraph = ox.load_graphml("./savedGraphs/" + place + ".graphml")
+else:
+    graph1 = ox.load_graphml("./savedGraphs/" + place + "1.graphml")
+    graph2 = ox.load_graphml("./savedGraphs/" + place + "2.graphml")
+    graph3 = ox.load_graphml("./savedGraphs/" + place + "3.graphml")
+    graph = nx.compose_all([graph1, graph2, graph3])
 
 #get edges and map with color dep. on length
 edges = ox.graph_to_gdfs(graph, nodes=False, edges=True)
